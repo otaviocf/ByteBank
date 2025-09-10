041337e29faf915f83d799a14f63a14217300fc6
otavio <proj.otaviocf@gmail.com>
2025-09-10 17:12:12 -0300

    updated Balance component to fetch balance fromn backend

--- src/Balance.jsx
+++ src/Balance.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import './Balance.css'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 function Balance() {
-	let balance = 200
+	async function getBalance() {
+		const res = await fetch('http://127.0.0.1:3000')
+		const data = await res.json()
+
+		return data.balance
+	}
+
+	useEffect(() => {
+		async function fetchBalance() {
+			const result = await getBalance()
+			console.log(result)
+			setBalance(result)
+		}
+		fetchBalance()
+	}, [])
+
+	const [balance, setBalance] = useState(null)
 
 	return (
 		<div className='balance-card'>
 			<p className='credit-balance'>Credit Balance</p>
-			<h2 className='balance-amount'>R$ {balance}</h2>
+			<h2 className='balance-amount'>{`R$ ${
+				balance !== null ? balance : 'Carregando...'
+			}`}</h2>
 			<div className='add-button-container'>
 				<button className='add-button add-hours'>Add Hours</button>
 				<button className='add-button add-classes'>Add Classes</button>
