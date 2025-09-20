3a8963815ce595d747a08da9af0c7ba846150716
otavio <proj.otaviocf@gmail.com>
2025-09-20 19:46:46 -0300

    updated logic for Balance component

--- src/Balance.jsx
+++ src/Balance.jsx
@@ -22,6 +22,20 @@ function Balance() {
 
 	const [balance, setBalance] = useState(null)
 
+	async function addHours(amount) {
+		const data = {
+			hours: amount,
+		}
+
+		const response = await fetch('http://127.0.0.1:3000', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+	}
+
 	return (
 		<div className='balance-card'>
 			<p className='credit-balance'>Credit Balance</p>
@@ -29,7 +43,9 @@ function Balance() {
 				balance !== null ? balance : 'Carregando...'
 			}`}</h2>
 			<div className='add-button-container'>
-				<button className='add-button add-hours'>Add Hours</button>
+				<button className='add-button add-hours' onClick={addHours}>
+					Add 1 Hour
+				</button>
 				<button className='add-button add-classes'>Add Classes</button>
 			</div>
 		</div>
